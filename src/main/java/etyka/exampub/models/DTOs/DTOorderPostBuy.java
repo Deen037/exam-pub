d6a658af9dc0ae6bb37921ce146d1fb4384bb3c5
Deen037 <deen.bds@gmail.com>
2023-10-26 11:46:27 +0200

    add order DTO for post buy endpoint

---
+++ src/main/java/etyka/exampub/models/DTOs/DTOorderPostBuy.java
@@ -0,0 +1,13 @@
+package etyka.exampub.models.DTOs;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class DTOorderPostBuy {
+
+    private Long userId;
+    private Long productId;
+    private double price;
+
+
+}
